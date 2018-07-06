856884fa7c043a82ef39429704673c2f52a1ef23
Nagarajan CK <nagainvertibrate@gmail.com>
2018-07-06 18:21:15 +0530

    Add markers

---
+++ js/app.js
@@ -0,0 +1,58 @@
+var places = [
+  { name: 'Bangalore Palace',
+    location: {lat : 12.998696, lng : 77.592026}
+  },
+  { name: 'Cubbon Park',
+  location: {lat : 12.976347, lng : 77.592928}
+  },
+  { name: 'Lal Bagh',
+  location: {lat : 12.950743, lng : 77.584777}
+  },
+  { name: 'Tipu Sultan Palace',
+  location: {lat : 12.959342, lng : 77.573625}
+  },
+  { name: 'Vidhana Soudha',
+  location: {lat : 12.979462, lng : 77.590909}
+}];
+
+
+
+var initMap = function() {
+  var map;
+  map = new google.maps.Map(document.getElementById('map'),{
+          center: { lat:12.97,lng:77.58},
+          zoom:13
+        });
+  var marker;
+  var markers = [];
+  for (var i = 0; i< places.length; i++){
+    var coordinates = places[i].location;
+    var name = places[i].name;
+    marker = new google.maps.Marker({
+      map: map,
+      position : coordinates,
+      title : name,
+      animation : google.maps.Animation.DROP,
+      id : i
+    });
+    markers.push(marker);
+    //add marker click funtion here
+  }
+}
+
+
+var ViewModel = function(){
+  var self = this;
+  this.placesList = ko.observableArray([]);
+  places.forEach(function(place){
+    self.placesList.push(new Model(place));
+}
+
+var Model = function(place){
+  this.name = ko.observable(place.name);
+  this.location = ko.observable(place.location);
+  //filtered goes here
+}
+
+
+ko.applyBindings(new ViewModel());
