6a622fa4b71d8132faf9a82db51c384b704b21e7
Nagarajan CK <nagainvertibrate@gmail.com>
2018-07-10 23:07:56 +0530

    Add readme file

---
+++ README.md
@@ -0,0 +1,19 @@
+# NEIGHBORHOOD MAP
+
+## ABOUT
+
+Neighborhood map lets you locate your favourite places in your neighborhood by rendering them on a map which uses the Google Maps API and the Wikipedia API to get more details about the place. The app also lets you filter the places easily.
+
+Regardless of what device you use to view the application, it renders accordingly to give a rich user experience.
+
+## REQUIREMENTS
+
+The application requires a latest internet browser and a working internet connection to load the map and also retrieve Wikipedia articles about it.
+
+## RUNNING THE APP
+
+To run the application, navigate to the __neighborhood_map__ and open the **index.html** file using your favourite browser. Ensure that your internet connection is seamless so that the user experience is not compromised.
+
+## CREDITS
+
+The application uses code from Google maps and Wikipedia APIs to run the application. The application also uses a code inspired from Stackoverflow to filter the places. Credits have been given in the code appropriately.
--- js/app.js
+++ js/app.js
@@ -28,12 +28,13 @@ var places = [
 var markers = [];
 var map;
 var initMap = function() {
+  // initMap is the callback function when the map has loaded, and renders the map initially.
   map = new google.maps.Map(document.getElementById('map'),{
           center: { lat:12.997,lng:77.58},
           zoom:11.8
         });
   ko.applyBindings(new ViewModel());
-  }
+};
 
 
 // init map applies bindings on the ViewModel once the map has been loaded
@@ -46,35 +47,31 @@ var ViewModel = function(){
   self.details = ko.observable('');
   self.extras = ko.observable('');
 
-
 // creates an observableArray containing which contains the places model objects
   places.forEach(
     function(place){
     self.placesList.push(new Model(place));
   });
 
-
 // filtered places contains the list of places models filtered according to user input
   self.filteredPlaces = ko.computed(function () {
     if(self.query()==undefined) {  // returns the placesList itself if no input
       return self.placesList();
     }
-    else{
+    else {
       arr =  self.placesList()
         .filter(placeItem => placeItem.name().toLowerCase().indexOf(self.query().toLowerCase()) > -1);
-      // filtering code source: stackoverflow. The places list is filtered upon the condition that
+      // filtering code source: Stackoverflow. The places list is filtered upon the condition that
       // updates the items such that only places that are superset of the input is returned
 
       // removes the initial markers from the map and nullifies markers list
       for (var i = 0; i < markers.length; i++) {
          markers[i].setMap(null);
       }
-      markers=[]
-
-
+      markers=[];
       var marker;
       // creates new markers according to the filtered list
-      for (var i = 0; i< arr.length; i++){
+      for (var i = 0; i< arr.length; i++) {
         var coordinates = arr[i].location();
         var name = arr[i].name();
         marker = new google.maps.Marker({
@@ -85,7 +82,6 @@ var ViewModel = function(){
           id : i
         });
         markers.push(marker);
-
         // once the markers are clicked, it is bounced and the wikipedia details are fetched
         marker.addListener('click',function () {
           this.setAnimation(4);
@@ -100,40 +96,38 @@ var ViewModel = function(){
 
   // function sets animation and gets wiki details when a list item is clicked
   this.locationTracker = function (data) {
-    for(var i=0; i<markers.length; ++i){
-      if(markers[i].title == data.name()){
+    for(var i=0; i<markers.length; ++i) {
+      if(markers[i].title == data.name()) {
         markers[i].setAnimation(4);
         break;
       }
     }
     clickedPlace = data.name();
     self.retrieveDetails(clickedPlace);
-  }
+  };
 
 
 // function asyncronously gets wikipedia details about a place and renders it in DOM
-this.retrieveDetails = function (place){
-
+  this.retrieveDetails = function (place) {
     var wikiurl = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&callback=wikiCallback&search='+place;
-    self.wikiDetail('Wikipedia articles about '+place)
+    self.wikiDetail('Wikipedia articles about '+place);
     var wikiRequestTimeout = setTimeout(function () {
-      self.wikiDetail('Sorry, Wikipedia articles could not be loaded.')
+      self.wikiDetail('Sorry, Wikipedia articles could not be loaded.');
     }, 5000);
     $.ajax(wikiurl, {
       dataType: "jsonp",
       success : function (response) {
         self.wikiurl(response[3][0]);
         self.details(response[0]);
-        self.extras(response[2][0])
-    clearTimeout(wikiRequestTimeout); // timeout created to display error is cleared
-  }
+        self.extras(response[2][0]);
+      clearTimeout(wikiRequestTimeout); // timeout created to display error is cleared
+      }
     });
-
-}
-}
+  };
+};
 
 // Model creates the name and location of a place as observable arrays
-var Model = function(place){
+var Model = function(place) {
   this.name = ko.observable(place.name);
   this.location = ko.observable(place.location);
-}
+};
